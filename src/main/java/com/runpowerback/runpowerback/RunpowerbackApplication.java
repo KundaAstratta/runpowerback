08bae643f5b2d265ae1ebbfe5176734061edca24
KundaAstratta <krystof.humbert@gmail.com>
2020-01-16 23:04:36 +0100

    first commit

---
+++ src/main/java/com/runpowerback/runpowerback/RunpowerbackApplication.java
@@ -0,0 +1,13 @@
+package com.runpowerback.runpowerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunpowerbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunpowerbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/runpowerback/runpowerback/RunpowerbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runpowerback.runpowerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunpowerbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
